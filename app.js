96c19b4a86f65765326ad9e2b14ef6cc1a62b9f1
lobZter <lobst3rd.cs01@nctu.edu.tw>
2016-08-19 07:05:08 +0000

    edit app.js

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 var express = require("express");
 var app = express();
 var http = require("http").Server(app);
+var https = require("https");
+var apikey = "AIzaSyDtFJs_awDvlStbCYz1PRqGCaq_beLkWjM";
 
 app.use(express.static(__dirname + "/public"));
 
 http.listen(process.env.PORT, process.env.IP);
+
+app.get('/', function(req, res) {
+    res.sendFile( __dirname + "/" + "index.htm" );
+});
+app.get('/search', function(req, res) {
+    var keyword = req.query.keyword;
+    var radius = req.query.radius;
+    var location = "25.0216697,121.5331069";
+    
+    var options = {
+        host: 'maps.googleapis.com',
+        path: '/maps/api/place/nearbysearch/json?types=restaurant&language=zh-TW&query=' + keyword + "&radius=" + radius + "&key=" + apikey + "&location=" + location
+    };
+    var callback = function(response) {
+        var str = "";
+        
+        response.on('data', function (chunk) {
+            str += chunk;
+        });
+        response.on('end', function () {
+            console.log(str);
+        });
+    }
+    https.get(options, callback);
+});
